9dc99c9794f85095a40cb74b9a13724097badeab
brunoprado <bruno.daraujo@sptech.school>
2025-10-22 13:47:16 -0300

    Adicionando prova

--- src/main/java/sptech/school/Laboratorio.java
+++ src/main/java/sptech/school/Laboratorio.java
@@ -1,5 +1,128 @@
-package school.sptech;
+package sptech.school;
+import school.sptech.exception.ArgumentoInvalidoException;
+import school.sptech.exception.VacinaInvalidaException;
+import school.sptech.exception.VacinaNaoEncontradaException;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Laboratorio {
 
+    private String nome;
+    private List<Vacina> vacinas;
+
+    public Laboratorio(String nome, List<Vacina> vacinas) {
+        this.nome = nome;
+        this.vacinas = new ArrayList<>();
+    }
+
+    public void adicionarVacina(Vacina vacina){
+        if(getVacinas().isEmpty()){
+            throw new VacinaInvalidaException("ERRO!");
+        }
+        if(vacina.getCodigo().isEmpty() || vacina.getCodigo().isBlank()){
+            throw new VacinaInvalidaException("ERRO!");
+        }
+        if(vacina.getNome().isEmpty() || vacina.getNome().isBlank()){
+            throw new VacinaInvalidaException("ERRO!");
+        }
+        if(vacina.getTipo().isEmpty() || vacina.getTipo().isBlank()){
+            throw new VacinaInvalidaException("ERRO!");
+        }
+        if(vacina.getEficacia() == null || (vacina.getEficacia() > 5 || vacina.getEficacia() < 0)){
+            throw new VacinaInvalidaException("ERRO!");
+        }
+        if(vacina.getDataLancamento() == null || vacina.getDataLancamento().isAfter(vacina.getDataLancamento())){
+            throw new VacinaInvalidaException("ERRO!");
+        }
+
+        vacinas.add(vacina);
+
+    }
+
+    public Vacina buscarVacinaPorCodigo(String codigo){
+        if(codigo == null || codigo.isBlank() || codigo.isEmpty()){
+            throw new ArgumentoInvalidoException("ERRO!");
+        }
+
+        Vacina vacinacerta = null;
+        for (Vacina v : vacinas) {
+            if(v.getCodigo().equals(codigo)){
+                vacinacerta = v;
+            }
+        }
+        if(vacinacerta == null){
+            throw new VacinaNaoEncontradaException("ERRO!");
+        }
+        return vacinacerta;
+    }
+
+    public void removerVacinaPorCodigo(String codigo){
+        if(codigo == null || codigo.isBlank() || codigo.isEmpty()){
+            throw new ArgumentoInvalidoException("ERRO!");
+        }
+
+        Vacina vacinaremover = null;
+        for (Vacina v : vacinas) {
+            if(v.getCodigo().equals(codigo)){
+                vacinaremover = v;
+                getVacinas().remove(vacinaremover);
+            }
+        }
+        if(vacinaremover == null){
+            throw new VacinaNaoEncontradaException("ERRO!");
+        }
+
+    }
+
+    public Vacina buscarVacinaComMelhorEficacia(){
+        if(getVacinas().isEmpty()){
+            throw new VacinaNaoEncontradaException("ERRO!");
+        }
+        Vacina melhor = null;
+        for (Vacina v : vacinas){
+        melhor = vacinas.getFirst();
+            if(v.getEficacia() > melhor.getEficacia()){
+                melhor = v;
+            }
+            if(v.getEficacia().equals(melhor.getEficacia())){
+                if(v.getDataLancamento().isBefore(melhor.getDataLancamento())){
+                melhor = v;
+                }
+            }
+        }
+        if(melhor == null){
+            throw new VacinaNaoEncontradaException("ERRO!");
+        }
+        return melhor;
+    }
+
+    public List<Vacina> buscarVacinaPorPeriodo(LocalDate dataInicio, LocalDate dataFim){
+        if(dataInicio == null || dataFim == null || (dataFim.isBefore(dataInicio))){
+            throw new ArgumentoInvalidoException("ERRO!");
+        }
+
+        List<Vacina> listaVacinas = null;
+        for (Vacina v : vacinas){
+            if((v.getDataLancamento().isBefore(dataFim) && v.getDataLancamento().isAfter(dataInicio))
+            || (v.getDataLancamento().isEqual(dataFim) || v.getDataLancamento().isEqual(dataInicio))){
+                listaVacinas.add(v);
+            }
+        }
+        return listaVacinas;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Vacina> getVacinas() {
+        return vacinas;
+    }
 }
--- src/main/java/sptech/school/Vacina.java
+++ src/main/java/sptech/school/Vacina.java
@@ -1,5 +1,81 @@
-package school.sptech;
+package sptech.school;
+
+import school.sptech.exception.ArgumentoInvalidoException;
+
+import java.time.LocalDate;
 
 public class Vacina {
 
+    private String codigo, nome, tipo;
+    private Double preco, eficacia;
+    private LocalDate dataLancamento;
+
+    public Vacina(String codigo, String nome, String tipo, Double preco, Double eficacia, LocalDate dataLancamento) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.preco = preco;
+        this.eficacia = eficacia;
+        if(this.eficacia < 0 || this.eficacia > 100) {
+            throw new ArgumentoInvalidoException("ERRO!");
+        }
+        this.dataLancamento = dataLancamento;
+    }
+
+    public String getEficaciaDescricao(){
+        String mensagem;
+        if(this.eficacia >= 90.5) mensagem = "EXCELENTE";
+        else if (this.eficacia >= 75.5)mensagem = "BOM";
+        else if (this.eficacia >= 50.5)mensagem = "REGULAR";
+        else mensagem = "RUIM";
+        return mensagem;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Double getEficacia() {
+        return eficacia;
+    }
+
+    public void setEficacia(Double eficacia) {
+        this.eficacia = eficacia;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
 }
